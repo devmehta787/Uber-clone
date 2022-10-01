a1fa4067c0dfa9d4a8a2c69ae50454e6c93c5fa0
devmehta787 <dev.mehta2019@vitstudent.ac.in>
2022-10-01 19:36:07 +0530

    confirm page + map fetching location using API

---
+++ pages/components/RideSelector.js
@@ -0,0 +1,53 @@
+import React from "react";
+import tw from "tailwind-styled-components";
+import { carList } from "../data/carList";
+
+function RideSelector() {
+  return (
+    <Wrapper>
+      <Title>Choose a ride, or Swipe up for more</Title>
+      <CarList>
+        {carList.map((car, index) => (
+          <Car key={index}>
+            <CarImage src={car.imgUrl} />
+            <CarDetails>
+              <Service>{car.service}</Service>
+              <Time>5 min</Time>
+            </CarDetails>
+            <Price>₹ 450</Price>
+          </Car>
+        ))}
+      </CarList>
+    </Wrapper>
+  );
+}
+
+export default RideSelector;
+
+const Wrapper = tw.div`
+flex-1 bg-white overflow-y-scroll flex flex-col
+`;
+const Title = tw.div`
+text-gray-500 text-center text-xs py-2 border-b
+`;
+const CarList = tw.div`
+overflow-y-scroll
+`;
+const Car = tw.div`
+flex p-4 itms-center
+`;
+const CarImage = tw.img`
+h-14 mr-4
+`;
+const CarDetails = tw.div`
+flex-1
+`;
+const Service = tw.div`
+font-medium 
+`;
+const Time = tw.div`
+text-xs text-blue-500
+`;
+const Price = tw.div`
+text-sm
+`;
--- pages/confirm.js
+++ pages/confirm.js
@@ -1,13 +1,21 @@
 import tw from "tailwind-styled-components";
+import { React, useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import Map from "./components/Map";
-import { useEffect, useState } from "react";
+import RideSelector from "./components/rideSelector";
+import Link from "next/link";
 
 const Confirm = () => {
+  const router = useRouter();
+  const { pickup, dropoff } = router.query;
+  console.log("Pickup:", pickup);
+  console.log("DropOff:", dropoff);
+
   const [pickupCoordinates, setPickupCoordinates] = useState();
   const [dropoffCoordinates, setDropoffCoordinates] = useState();
 
-  const getPickupCoordinates = () => {
-    const pickup = "Jamshedpur";
+  const getPickupCoordinates = (pickup) => {
+    // const pickup = "Jamshedpur";
     fetch(
       `https://api.mapbox.com/geocoding/v5/mapbox.places/${pickup}.json?` +
         new URLSearchParams({
@@ -24,8 +32,8 @@ const Confirm = () => {
       });
   };
 
-  const getDropoffCoordinates = () => {
-    const dropoff = "Chennai";
+  const getDropoffCoordinates = (dropoff) => {
+    // const dropoff = "Chennai";
     fetch(
       `https://api.mapbox.com/geocoding/v5/mapbox.places/${dropoff}.json?` +
         new URLSearchParams({
@@ -43,19 +51,28 @@ const Confirm = () => {
   };
 
   useEffect(() => {
-    getPickupCoordinates();
-    getDropoffCoordinates();
-  }, []);
+    getPickupCoordinates(pickup);
+    getDropoffCoordinates(dropoff);
+  }, [pickup, dropoff]);
 
   return (
     <Wrapper>
+      {/* <ButtonContainer> */}
+      <Link href="/">
+        <BackButton src="https://img.icons8.com/ios-filled/50/000000/left.png" />
+      </Link>
+      {/* </ButtonContainer> */}
       <Map
         pickupCoordinates={pickupCoordinates}
         dropoffCoordinates={dropoffCoordinates}
       />
       <RideContainer>
-        {pickupCoordinates}
-        {dropoffCoordinates}
+        {/* {pickupCoordinates}
+        {dropoffCoordinates} */}
+        <RideSelector />
+        <ConfirmButtonContainer>
+          <ConfirmButton>Confirm UberX</ConfirmButton>
+        </ConfirmButtonContainer>
       </RideContainer>
     </Wrapper>
   );
@@ -66,7 +83,18 @@ export default Confirm;
 const Wrapper = tw.div`
 flex h-screen flex-col
 `;
-
+// const ButtonContainer = tw.div`
+// bg-white px-4
+// `;
+const BackButton = tw.img`
+w-10 h-10 bg-gray-200 rounded-full ml-3 cursor-pointer hover:scale-110 transition
+`;
 const RideContainer = tw.div`
-flex-1
+flex-1 flex flex-col h-1/2
+`;
+const ConfirmButtonContainer = tw.div`
+border-t-2
+`;
+const ConfirmButton = tw.div`
+flex flex-col bg-black text-white text-xl items-center mt-10 py-3 mx-4 cursor-pointer transform rounded-md
 `;
